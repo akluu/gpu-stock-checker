228a06e1a4a28a28da23540ff53b120ae7f48103
akluu <81354105+akluu@users.noreply.github.com>
2022-05-30 13:15:55 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,34 +8,35 @@ API_KEY = "?apiKey=" + os.environ.get("BBKEY")
 OPTIONS = "&sort=onlineAvailability.asc&show=onlineAvailability,addToCartUrl&format=json"
 full_link = BASE_URL + API_KEY + OPTIONS
 
-response = requests.get(full_link)
-print(response.status_code)
-print(response.json())
+# print(os.environ.get("BBKEY"))
+# response = requests.get(full_link)
+# print(response.status_code)
+# print(response.json())
 
-# async def getResponse():
-#     while True:
-#         response = requests.get(full_link)
-#         if response.status_code != 200:
-#             await asyncio.sleep(60)
-#             continue
-#         else:
-#             return response
+async def getResponse():
+    while True:
+        response = requests.get(full_link)
+        if response.status_code != 200:
+            await asyncio.sleep(60)
+            continue
+        else:
+            return response
 
-# async def main():
-#         in_stock = False
-#         attempts = 0
-#         while(not in_stock):
-#             try:
-#                 task = asyncio.create_task(getResponse())
-#                 response = await asyncio.wait_for(task, timeout = 60*60*24)
-#                 in_stock = response.json()['products'][0]['onlineAvailability']
-#                 if(not in_stock):
-#                         print('Time=' + str(datetime.now()) + '- Attempt=' + str(attempts))
-#                         attempts += 1
-#                 await asyncio.sleep(3)
-#             except asyncio.TimeoutError:
-#                 print("Bad Status Code")
-#                 continue
+async def main():
+        in_stock = False
+        attempts = 0
+        while(not in_stock):
+            try:
+                task = asyncio.create_task(getResponse())
+                response = await asyncio.wait_for(task, timeout = 60*60*24)
+                in_stock = response.json()['products'][0]['onlineAvailability']
+                if(not in_stock):
+                        print('Time=' + str(datetime.now()) + '- Attempt=' + str(attempts))
+                        attempts += 1
+                await asyncio.sleep(3)
+            except asyncio.TimeoutError:
+                print("Bad Status Code")
+                continue
 
-# asyncio.run(main())
+asyncio.run(main())
 
