278e5b79860876bee66e0b47c7da9536c823f09a
akluu <81354105+akluu@users.noreply.github.com>
2022-05-31 00:36:40 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
 import requests
 import asyncio
+import boto3
 from datetime import datetime
 
 BASE_URL = "https://api.bestbuy.com/v1/products(sku in(6429440))"
 API_KEY = "?apiKey=" + os.environ.get("BBKEY")
-OPTIONS = "&sort=onlineAvailability.asc&show=onlineAvailability,addToCartUrl&format=json"
+OPTIONS = "&sort=onlineAvailability.asc&show=onlineAvailability,addToCartUrl,onlineAvailabilityUpdateDate&format=json"
 full_link = BASE_URL + API_KEY + OPTIONS
 
 async def getResponse():
@@ -17,6 +18,28 @@ async def getResponse():
         else:
             return response
 
+def publishStock(url):
+    arn = 'arn:aws:sns:us-west-2:729495428235:stock_alert'
+    sns_client = boto3.client(
+        'sns',
+        aws_access_key_id=os.environ.get("AWSAK"),
+        aws_secret_access_key=os.environ.get("AWSSAK"),
+        region_name='us-west-2'
+    )
+    response = sns_client.publish(
+        TopicArn=arn, Message="GPUS in Stock! Link: " + url)
+
+def publishError():
+    arn = 'arn:aws:sns:us-west-2:729495428235:code_broken_alert'
+    sns_client = boto3.client(
+        'sns',
+        aws_access_key_id=os.environ.get("AWSAK"),
+        aws_secret_access_key=os.environ.get("AWSSAK"),
+        region_name='us-west-2'
+    )
+    response = sns_client.publish(
+        TopicArn=arn, Message="Code caught an error.")
+
 async def main():
         in_stock = False
         attempts = 0
@@ -25,12 +48,17 @@ async def main():
                 task = asyncio.create_task(getResponse())
                 response = await asyncio.wait_for(task, timeout = 60*60*24)
                 in_stock = response.json()['products'][0]['onlineAvailability']
+                last_time_in_stock = response.json()['products'][0]['onlineAvailabilityUpdateDate']
+                url = response.json()['products'][0]['addToCartUrl']
                 if(not in_stock):
-                        print('Time=' + str(datetime.now()) + '- Attempt=' + str(attempts))
+                        print('Time=' + str(datetime.now()) + '- Attempt=' + str(attempts) + '- LastTimeInStock=' + str(last_time_in_stock))
                         attempts += 1
+                else:
+                    publishStock(url)
                 await asyncio.sleep(3)
             except asyncio.TimeoutError:
                 print("Bad Status Code")
+                publishError()
                 continue
 
 asyncio.run(main())
